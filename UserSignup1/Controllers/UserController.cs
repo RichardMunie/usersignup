500a628c894c6fe5a4f4645d1df6073a23f0956e
John Kautzner <misterkautzner@gmail.com>
2018-06-05 18:33:54 -0500

    Ready to start studio 9

---
+++ UserSignup1/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserSignup1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserSignup1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            if((user.Password != null && verify != null) && verify == user.Password)
+            {
+
+                ViewBag.message = "Welcome, " + user.Username;
+                return View("Index");
+            }
+ 
+            ViewBag.message = "Passwords do not match";
+            user.Password = string.Empty;
+            ViewBag.user = user;
+            return View();
+            
+        }
+    }
+}
---
+++ UserSignup1/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup1.Models
+{
+    public class User
+    {
+        [MaxLength(25)]
+        public string Username { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [MaxLength(25)]
+        public string Password { get; set; }
+
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+
+        public User() { }
+    }
+}
